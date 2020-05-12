92aaae4905c1392562da3caeb8bd6ca531172b76
Sri Datta <b.sridatta@gmail.com>
2020-05-12 21:23:46 +0200

    exp pin mem

--- src/train.py
+++ src/train.py
@@ -157,7 +157,7 @@ def training_specific_args():
                         help='choose variant, the combination of VAEs to be trained')
     parser.add_argument('--latent_dim', default=30, type=int,
                         help='dimensions of the cross model latent space')
-    parser.add_argument('--beta', default=10**-2, type=float,
+    parser.add_argument('--beta', default=0.001, type=float,
                         help='KLD weight')
     parser.add_argument('--pretrained', default=False, type=lambda x: (str(x).lower() == 'true'),
                         help='use pretrained weights for RGB encoder')
