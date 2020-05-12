f8036bea0de20c4d294eaa9f04f9d846f1a047b5
Sri Datta <b.sridatta@gmail.com>
2020-05-12 15:50:32 +0200

    remove mayavi

--- src/viz.py
+++ src/viz.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
-from mayavi import mlab
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
@@ -73,42 +72,3 @@ def plot_diff(pose, target, error):
     #     ax.view_init(30, angle)
     #     plt.draw()
     #     plt.pause(.001)
-
-
-def plot_diff_3D(pose, target, error):
-    # info to connect joints
-    skeleton = ((0, 7), (7, 8), (8, 9), (9, 10), (8, 11), (11, 12), (12, 13),
-                (8, 14), (14, 15), (15, 16), (0, 1), (1, 2), (2, 3), (0, 4), (4, 5), (5, 6))
-
-    black = (0, 0, 0)
-    white = (1, 1, 1)
-    mlab.figure(bgcolor=white)
-
-    x = pose[:, 0]
-    y = -1*pose[:, 2]
-    z = -1*pose[:, 1]
-
-    x1 = target[:, 0]
-    y1 = -1*target[:, 2]
-    z1 = -1*target[:, 1]
-
-    for link in skeleton:
-        x_ = [x[link[0]], x[link[1]]]
-        y_ = [y[link[0]], y[link[1]]]
-        z_ = [z[link[0]], z[link[1]]]
-        mlab.plot3d(x_, y_, z_, color=(0, 0, 1), tube_radius=10.)
-
-        x1_ = [x1[link[0]], x1[link[1]]]
-        y1_ = [y1[link[0]], y1[link[1]]]
-        z1_ = [z1[link[0]], z1[link[1]]]
-        mlab.plot3d(x1_, y1_, z1_, color=(0, 0, 1), tube_radius=10., opacity=0.5)
-
-
-    # Finally, display the set of lines
-    # mlab.pipeline.surface(lines, colormap='Accent', line_width=1, opacity=.4)
-
-    # And choose a nice view
-    # mlab.view(33.6, 106, 5.5, [0, 0, .05])
-    # mlab.roll(125)
-    # mlab.savefig("stick_2.obj")
-    mlab.show()
