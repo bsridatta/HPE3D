72b3c14739d130ab1233edf2d1c9ffebb8246d5f
Sri Datta <b.sridatta@gmail.com>
2021-02-26 16:50:45 +0100

    16j branch

--- src/datasets/common.py
+++ src/datasets/common.py
@@ -18,7 +18,7 @@ COMMON_JOINTS[12] = 'L_Wrist'
 COMMON_JOINTS[13] = 'R_Shoulder'
 COMMON_JOINTS[14] = 'R_Elbow'
 COMMON_JOINTS[15] = 'R_Wrist'
-
+# with spine - 16 joints
 
 JOINT_CONNECTIONS = (('Pelvis', 'Torso'), ('Torso', 'Neck'), ('Neck', 'Head'), ('Neck', 'L_Shoulder'), ('L_Shoulder', 'L_Elbow'), ('L_Elbow', 'L_Wrist'), ('Neck', 'R_Shoulder'), (
             'R_Shoulder', 'R_Elbow'), ('R_Elbow', 'R_Wrist'), ('Pelvis', 'R_Hip'), ('R_Hip', 'R_Knee'), ('R_Knee', 'R_Ankle'), ('Pelvis', 'L_Hip'), ('L_Hip', 'L_Knee'), ('L_Knee', 'L_Ankle'))
